b9f6a61bcfbdeabfe381880043a1cab63be3a53b
filka657 <72990504+filka657@users.noreply.github.com>
2022-07-16 01:36:44 +0300

    Add files via upload

--- p3.cpp
+++ p3.cpp
@@ -37,6 +37,7 @@ public:
 	double m01;
 	double m10;
 	double area;
+	float min_d;
 
 	Markers(string color_str, Scalar left, Scalar right);
 	void detect(Mat picture, Scalar left, Scalar right);
@@ -44,27 +45,27 @@ public:
 	void draw(Mat background);
 	void momentss();
 	void point(Mat background);
-	void draw_line(Mat background);
+	void draw_line(Mat background, Point last);
 	void text(Mat background);
-	void show(Mat background);
+	void show(Mat background, Mat robo_line);
+	void text_distance(Mat background, float d);
 };
 
 Markers::Markers(string color_str, Scalar left, Scalar right) {
 	this->color_str = color_str;
 	this->left = left;
 	this->right = right;
+	this->min_d = 1000;
 }
 
 void Markers::detect(Mat picture, Scalar left, Scalar right)
 {
 	inRange(picture, left, right, mask);
-	imshow("mask", mask);
 }
 
 void Markers::find()
 {
 	findContours(mask, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-	imshow("Mask", mask);
 }
 
 void Markers::draw(Mat background)
@@ -95,27 +96,30 @@ void Markers::point(Mat background)
 	circle(background, center, 1, (0, 255, 0), 2);
 }
 
-void Markers::draw_line(Mat background)
+void Markers::draw_line(Mat background, Point last)
 {
-
+	line(background, center, last, Scalar(0, 0, 255), 2);
 }
 
 void Markers::text(Mat background)
 {
 	coordinates = to_string(center.x) + "," + to_string(center.y);
 	putText(background, coordinates, Point(center.x + 5, center.y - 10), FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0));
-	
 }
 
-void Markers::show(Mat background)
+void Markers::show(Mat background, Mat robo_line)
 {
-	return imshow("background", background);
+	return imshow("background", background + robo_line);
+}
+
+void Markers::text_distance(Mat background, float d)
+{
+	putText(background, "d: " + to_string(d), Point(center.x + 5, center.y + 10), FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0));
 }
 
 int main()
 {
 	VideoCapture cap(0);
-	vector<vector<float>> coloring = {};
 
 	Markers R("red", red_left, red_right);
 	Markers Y("yellow", yellow_left, yellow_right);
@@ -126,9 +130,14 @@ int main()
 	
 	if (!cap.isOpened()) cout << "Can not open the camera" << endl;
 
-	int x_last = -1;
-	int y_last = -1;
+	Point last;
+	last.x = -1;
+	last.y = -1;
 
+	Mat robo_line;
+	cap >> robo_line;
+	Mat imgLines = Mat::zeros(robo_line.size(), CV_8UC3);;
+	float min_distance(1500.0);
 	while (1)
 	{
 		Mat video;
@@ -138,6 +147,7 @@ int main()
 			Mat background;
 			video.copyTo(background);
 			cvtColor(video, video, COLOR_BGR2HSV);
+			float distance;
 			for (int i = 0; i < 5; i++)
 			{
 				mark[i].detect(video, mark[i].left, mark[i].right);
@@ -145,8 +155,29 @@ int main()
 				mark[i].draw(background);
 				mark[i].momentss();
 				mark[i].point(background);
+				if (i == 0)
+				{
+					if (last.x >= 0 && last.y >= 0 && mark[i].center.x >= 0 && mark[i].center.y >= 0)
+					{
+						mark[i].draw_line(robo_line, last);
+					}
+					last.x = mark[i].center.x;
+					last.y = mark[i].center.y;
+				}
 				mark[i].text(background);
-				mark[i].show(background);
+				mark[i].show(background, robo_line);
+				if (i != 0)
+				{
+					distance = sqrt(pow(R.center.x - mark[i].center.x, 2) + (pow(R.center.y - mark[i].center.y, 2)));
+					if (distance < mark[i].min_d)
+						mark[i].min_d = distance;
+					if (distance < 1000.0) {
+						cout << "check the " << mark[i].color_str << " marker" << endl;
+					}
+					mark[i].text_distance(background, distance);
+					if (distance < min_distance)
+						min_distance = distance;
+				}
 			}
 			char c = waitKey(33);
 			if (c == 27)
@@ -155,6 +186,10 @@ int main()
 				break;
 			}
 		
+	}
+	for (int i = 1; i < 5; i++)
+	{
+		cout << "minimal distance by " << mark[i].color_str << ": " << mark[i].min_d << endl;
 	}
 	waitKey(0);
 }

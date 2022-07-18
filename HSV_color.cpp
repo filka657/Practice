e575ea4398fb82254a9aebe37ccfadb62d2db5a5
filka657 <72990504+filka657@users.noreply.github.com>
2022-07-18 12:32:14 +0300

    Sticker HSV color detection

---
+++ HSV_color.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    VideoCapture cap(0); //Get the video
+
+    if (!cap.isOpened())  // if not opened, exit program
+    {
+        cout << "Cannot open the web cam" << endl;
+        return -1;
+    }
+
+    namedWindow("Control", WINDOW_AUTOSIZE); //create a window called "Control"
+
+    int iLowH = 170;
+    int iHighH = 179;
+
+    int iLowS = 150;
+    int iHighS = 255;
+
+    int iLowV = 60;
+    int iHighV = 255;
+
+    //Create trackbars in "Control" window
+    createTrackbar("LowH", "Control", &iLowH, 179);
+    createTrackbar("HighH", "Control", &iHighH, 179);
+
+    createTrackbar("LowS", "Control", &iLowS, 255);
+    createTrackbar("HighS", "Control", &iHighS, 255);
+
+    createTrackbar("LowV", "Control", &iLowV, 255);
+    createTrackbar("HighV", "Control", &iHighV, 255);
+
+    int iLastX = -1;
+    int iLastY = -1;
+
+    //Capture a temporary image from the camera
+    Mat imgTmp;
+    cap.read(imgTmp);
+
+    //Create a black image with the size as the camera output
+    Mat imgLines = Mat::zeros(imgTmp.size(), CV_8UC3);;
+
+    while (true)
+    {
+        Mat imgOriginal;
+        bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+
+        if (!bSuccess)
+        {
+            cout << "Cannot read a frame from video stream" << endl;
+            break;
+        }
+        Mat imgHSV;
+        //Convert the capture frame from BGR to HSV
+        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); 
+        
+        Mat imgThresholded;
+        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the imag
+        imshow("Thresholded Image", imgThresholded); //show the thresholded image
+
+        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+        {
+            cout << "esc key is pressed by user" << endl;
+            break;
+        }
+    }
+    return 0;
+}
